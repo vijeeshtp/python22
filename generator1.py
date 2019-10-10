764ef0819825076d0110dc111e115b54d199dcae
techehub <vijeesh.tp@techehub.com>
2019-10-10 10:50:03 +0530

    Generator 1 updated

--- generator1.py
+++ generator1.py
@@ -3,17 +3,21 @@ def myfunction():
     yield 2
     yield 3
 
-
+# executing generator -1
 x= iter(myfunction())
 print (next(x))
 print (next(x))
 print (next(x))
 
 
+
+# executing generator -2
 for x in myfunction():
     print (x)
 
 
+
+# executing generator -3
 x= iter(myfunction())
 while True :
     try:
